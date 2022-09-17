2dcd927f1f26bf98c0adf102050e3c2a2cc86cdc
sneha rao <sneharao300@gmail.com>
2022-09-18 01:26:03 +0530

    NewsApp | Added error block to handle getNewsApi Error and a loader

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,26 +12,38 @@ export class HomeComponent implements OnInit {
   public noOfDrafts: number;
   public noOfPublished: number;
   public noOfArchives: number;
+  public isError: boolean;
+  public isLoading: boolean;
+
   constructor(private boardsService: BoardsService) {
     this.boards = [];
     this.noOfDrafts = 0;
     this.noOfArchives = 0;
     this.noOfPublished = 0;
+    this.isError = false;
+    this.isLoading = true;
   }
 
   ngOnInit(): void {
     this.boardsService.retriveAllBoards().subscribe(data => {
+      this.isLoading = false;
       this.boards = data;
     });
   }
 
   onBoardItemClick(selectedBoardItem: Board) {
+    this.isLoading = true;
     this.boardsService.retrieveNewsByBoardId(selectedBoardItem.id).subscribe(data => {
-      console.log(data);
       const { drafts, published, archives } = data;
       this.noOfDrafts = drafts.length;
       this.noOfArchives = archives.length;
       this.noOfPublished = published.length;
+    }, error => {
+      console.log(error);
+      this.isLoading = false;
+      this.isError = true;
+    }, () => {
+      this.isLoading = false;
     });
   }
 
